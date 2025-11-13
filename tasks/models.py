45b3f2372df8e014397ac5a8b2ed72b0266fdda8
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-11-12 20:26:28 -0600

    added assigned_to field

--- tasks/models.py
+++ tasks/models.py
@@ -13,6 +13,13 @@ class Task(models.Model):
     description = models.TextField(blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='backlog')
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    assigned_to = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='tasks_assigned'
+    )
 
     def __str__(self):
         return self.title
