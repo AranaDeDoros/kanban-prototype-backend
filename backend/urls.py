3b9a3ef49c4712e16d8fc86af0d45f0d7bf9df7f
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-11-12 20:26:06 -0600

    routing for accounts

--- backend/urls.py
+++ backend/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/accounts/', include('accounts.urls')),
     path('api/projects/', include('projects.urls')),
     path('api/tasks/', include('tasks.urls')),
 ]
