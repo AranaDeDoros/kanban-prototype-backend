ed990a0075ab25c454d6b76c7306758226b0504e
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-11-29 15:44:00 -0600

    moving choices to a common path

---
+++ common/choices.py
@@ -0,0 +1,12 @@
+
+STATUS_CHOICES = [
+    ('backlog', 'Backlog'),
+    ('wip', 'In Progress'),
+    ('done', 'Completed'),
+]
+
+PRIORITIES = [
+    ('high', 'High'),
+    ('regular', 'Regular'),
+    ('low', 'Low'),
+]
